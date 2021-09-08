24635da924b4084a45f376faaccf0c70cf4f0306
MAFADA <afadanursaiva2001@gmail.com>
2021-09-08 13:32:11 +0700

    Penambahan Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "NIM: 2031710168,Nama: M. Afada Nur Saiva Syahira";
+    }
+}
